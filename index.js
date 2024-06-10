0f2ad0a465990c26cdd158c2d63f2f517db1cf8c
arafahmed7 <aaraf772@gmail.com>
2024-06-10 19:47:31 +0600

    vercel deployment completed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server (optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const userCollection = client.db("parcelDb").collection("user");
         const parcelCollection = client.db("parcelDb").collection("parcel");
         const reviewCollection = client.db("parcelDb").collection("reviews");
@@ -324,8 +324,8 @@ async function run() {
             }
         });
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
